d10e14387c3e5356da20acbe9c4fd96773948cf9
Osman <oozcanosman@gmail.com>
2024-01-13 03:34:27 +0300

    category chart

--- assets/javascripts/issue_lifecycle.js
+++ assets/javascripts/issue_lifecycle.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 		const data = await response.json();
 
 		let dataContainer = document.getElementById('dataContainer');
-		dataContainer.innerHTML = formatDataAsHTML(data);
+		dataContainer.innerHTML = await issueTableHTML(data.issues_data);
 
 		$(document).ready(function () {
 			new DataTable('#issuesTable', {
@@ -24,15 +24,25 @@ document.addEventListener('DOMContentLoaded', async function () {
 				}
 			});
 		});
+		createCategoryChart(data.issues_data);
+
+		dataContainer = document.getElementById('categoryTableContainer');
+		dataContainer.innerHTML = await categoryTableHTML(data.total_elapsed_time_category);
 	} catch (error) {
 		console.error("Data fetch Error!:", error);
 	}
 
-	function formatDataAsHTML(data) {
+	async function issueTableHTML(issues_data)
+	{
 		let html = '<table id="issuesTable" class="table-striped cell-border" style="width:100%">';
-		html += '<thead class="thead-dark" style="font-size:large;"><tr><th data-sortable="true">İş Numarası</th><th>Suanki Durum</th><th>Durumlar İçin Geçen Süre</th><th style="text-align:center;">Durum Değişiklikleri</th><th>Kategori</th></tr></thead>';
+		html += '<thead class="thead-dark" style="font-size:large;"><tr>';
+		html += '<th data-sortable="true">'+ translations.label_issue_number + '</th>';
+		html += '<th>' + translations.label_current_status + '</th>';
+		html += '<th>' + translations.label_elapsed_time_for_statuses + '</th>';
+		html += '<th style="text-align:center;">' + translations.label_status_changes + '</th>';
+		html += '<th>' + translations.label_category + '</th></tr></thead>';
 		html += '<tbody>';
-		data.issues_data.forEach(function (issue) {
+		issues_data.forEach(function (issue) {
 			html += '<tr class="table-row" data-issue=\'' + JSON.stringify(issue) + '\' > ';
 			html += '<td class="isId2">' + issue.id + '</td>';
 			html += '<td class="isId">' + (issue.status_now || '-') + '</td>';
@@ -52,7 +62,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 			html += '<td class="issue-status" style="text-align:center;" data-issue=\'' + JSON.stringify(issue.status_changes) + '\'>';
 			issue.status_changes.forEach(function (status_change, index) {
 				if (index < 3) {
-					html += status_change.from_status + ' <span style="font-size:x-large;">&#8594;</span> ' + status_change.to_status + ' ( <strong>' + status_change.changed_by + '</strong> tarafindan ) ' + ' <br>';
+					html += status_change.from_status + ' <span style="font-size:x-large;">&#8594;</span> ' + status_change.to_status + ' ( ' + translations.label_byen + '<strong>' + status_change.changed_by + '</strong> ' + translations.label_bytr + ') ' + ' <br>';
 				} else {
 					html += '<span class="additional-status" style="display: none;">' + status_change.from_status + ' <span style="font-size:x-large;">&#8594;</span> ' + status_change.to_status + ' <strong>(Değiştiren:</strong> ' + status_change.changed_by + ')<br></span>';
 				}
@@ -66,14 +76,18 @@ document.addEventListener('DOMContentLoaded', async function () {
 		});
 		html += '</tbody>';
 		html += '</table>';
-		//html += '<p class="mt-4">Toplam Geçen Süre (Kategori): ' + data.total_elapsed_time_category + ' saat</p>';
 		//html += '<p>User Spent Time: ' + data.user_spent_time + ' saat</p>';
+		return html;
+	}
 
+	async function categoryTableHTML(total_elapsed_time_category)
+	{
 		let categoryHtml = '<table class="table table-dark display table-striped" style="width:100%">';
-			categoryHtml += '<thead class="thead-dark"><tr><th class="thCl" data-sortable="true">Kategori</th><th class="thCl">İş ID\'leri</th><th class="thCl" data-sortable="true">Toplam Geçen Süre (Saat)</th></tr></thead>';
-			categoryHtml += '<tbody>';
-			console.log(data);
-			data.total_elapsed_time_category.forEach(function (category) {
+		categoryHtml += '<thead class="thead-dark"><tr><th class="thCl" data-sortable="true">' + translations.label_category + '</th>';
+		categoryHtml += '<th class="thCl">' + translations.label_issue_numbers + '</th>';
+		categoryHtml += '<th class="thCl" data-sortable="true">' + translations.label_total_elapsed_time + '</th></tr></thead>';
+		categoryHtml += '<tbody>';
+		total_elapsed_time_category.forEach(function (category) {
 				categoryHtml += '<td>' + (category.category || '-') + '</td>';
 				categoryHtml += '<td class="isId" data-issue=\'' + JSON.stringify(category.issue_ids) + '\'>';
 				category.issue_ids.forEach(function (id, index) {
@@ -88,61 +102,95 @@ document.addEventListener('DOMContentLoaded', async function () {
 				}
 				categoryHtml += '</td>'
 				categoryHtml += '<td class="totalHours">'
-					let formattedTime = formatTime(category.total_elapsed_hours, category.total_elapsed_minutes);
-					categoryHtml += formattedTime ;
+				let formattedTime = formatTime(category.total_elapsed_hours, category.total_elapsed_minutes);
+				categoryHtml += formattedTime ;
 				categoryHtml += '</td>';
 				categoryHtml += '</tr>';
 			});
 			categoryHtml += '</tbody>';
 			categoryHtml += '</table>';
 
-			document.getElementById('categoryTableContainer').innerHTML = categoryHtml;
-
-		return html;
+			// document.getElementById('categoryTableContainer').innerHTML = categoryHtml;
+			return categoryHtml;
 	}
+
+
+	async function createCategoryChart(issues)
+	{
+		let labels2 = [];
+
+		let issuesData = await Promise.all(issues.map(async issue => {
+			if (!labels2.some(label => label === issue.category))
+			labels2.push(issue.category);
+		return {
+				label: translations.label_issue + ' #' + issue.id,
+				category: issue.category,
+				duration: issue.total_elapsed_hours + Math.floor(issue.total_elapsed_minutes) / 60,
+				backgroundColor: await randomColors(null)
+			};
+		}));
+
+		const data2 = {
+			labels: labels2,
+			datasets: issuesData.map(issue => ({
+			label: issue.label,
+			data: labels2.map((label) => (label === issue.category ? issue.duration : 0)),
+			backgroundColor: [issue.backgroundColor],
+		})),
+		};
+
+		const options = {
+			plugins: {
+				title: {
+					display: true,
+					text: 'Chart.js Bar Chart - Stacked',
+				},
+			},
+			responsive: true,
+			maintainAspectRatio: false,
+			scales: {
+				x: {
+					stacked: true,
+				},
+				y: {
+					stacked: true,
+				},
+			},
+		}
+
+		createChart('bar', data2, options, null, 'categoryChart');
+	}
+
 
 	function formatTime(hours, minutes) {
 		let formattedTime = '';
-		if (hours > 0) {
-			formattedTime += hours + ' saat ';
+		if (minutes >= 60)
+		{
+			tmp = minutes;
+			hours += Math.floor(minutes / 60);
+			minutes = (tmp % 60);
 		}
-		formattedTime += minutes + ' dakika';
+		if (hours > 0) {
+			formattedTime += hours + translations.label_hour;
+		}
+		formattedTime += minutes + translations.label_minute;
 		return formattedTime;
 	}
 
-	async function createChart(labels, data, chartContainer, e)
+	async function createChart(type, data, options, chartContainer, element)
 	{
 		let ctx = document.createElement('canvas').getContext('2d');
-		ctx.canvas.style.width = '100%';
-		ctx.canvas.style.height = '100%';
-		ctx.canvas.style.display = 'block';
-		ctx.canvas.style.margin = 'auto';
-
-		chartContainer = await document.getElementById('issuesTable').insertRow(e.target.closest('tr').rowIndex + 1).insertCell(0);
-		chartContainer.colSpan = 4;
-		chartContainer.style.width = '100%'
-		chartContainer.style.height = '500px'
-		chartContainer.appendChild(ctx.canvas);
+		if (element === 'categoryChart')
+		document.getElementById('categoryChart').appendChild(ctx.canvas);
+	else
+			chartContainer.appendChild(ctx.canvas);
 
 		new Chart(ctx, {
-			type: 'pie',
-			data: {
-				labels: labels,
-				datasets: [{
-					data: data,
-					backgroundColor: ['#ff6384', '#36a2eb', '#cc65fe', '#ffce56', '#9CCC65', '#FFA726']
-				}]
-			},
-			options: {
-				responsive: true,
-				maintainAspectRatio: false,
-				legend: {
-					labels: {
-						fontSize: 50
-					}
-				}
-			}
+			type: type,
+			data: data,
+			options: options
 		});
+
 		return chartContainer;
 	}
 
@@ -183,11 +231,54 @@ document.addEventListener('DOMContentLoaded', async function () {
 			}
 			previousIssueId = currentIssueId;
 
-			chartContainer = await createChart(labels, data, chartContainer, e);
+			chartContainer = await document.getElementById('issuesTable').insertRow(e.target.closest('tr').rowIndex + 1).insertCell(0);
+			chartContainer.colSpan = 4;
+			chartContainer.style.width = '100%'
+			chartContainer.style.height = '500px'
+
+			data = {
+				labels: labels,
+				datasets: [{
+					data: data,
+					backgroundColor: await randomColors(labels)
+				}]
+			}
+
+			options = {
+				responsive: true,
+				maintainAspectRatio: false,
+				legend: {
+					labels: {
+						fontSize: 50
+					}
+				}
+			}
+
+			chartContainer = await createChart('pie', data, options, chartContainer);
 		}
 	});
 
 	$(document).on('click', '.show-more-btn', function () {
 		$('.additional-time, .additional-status').toggle();
 	});
+
+	async function randomColors(labels)
+	{
+		let rColor;
+		if (labels)
+		{
+			rColor = labels.map(() =>
+			'rgba(' +
+			Math.floor(Math.random() * 256) + ',' +
+			Math.floor(Math.random() * 256) + ',' +
+			Math.floor(Math.random() * 256) + ')');
+		}
+		else{
+			rColor = 'rgba(' +
+				Math.floor(Math.random() * 256) + ',' +
+				Math.floor(Math.random() * 256) + ',' +
+				Math.floor(Math.random() * 256) + ')'
+		}
+		return rColor;
+	}
 });
