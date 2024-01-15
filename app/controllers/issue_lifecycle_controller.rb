a5a8e0679ddfbf9b3b1fa8859ba67588d0279d9a
Osman <oozcanosman@gmail.com>
2024-01-15 03:28:28 +0300

    user table & user d.

--- app/controllers/issue_lifecycle_controller.rb
+++ app/controllers/issue_lifecycle_controller.rb
@@ -6,32 +6,33 @@ class IssueLifecycleController < ApplicationController
 		issues_data = []
 		total_elapsed_time_category = []
 		issues = @project.issues.includes(:journals)
-		user_spent_time = []
+		user_spent_time = project_details();
+		project_name = @project.name;
 
 		issues.each do |issue|
 		  issue_id = issue.id
 		  status = issue.status.name
 		  issue_category = issue.category.try(:name)
 		  issue_creation_time = issue.created_on
-		  user_ids = issue.journals.map { |journal| journal.user_id }.uniq
-		  puts "user_ids: #{user_ids}, issue_id: #{issue_id}"
+		#   user_ids = issue.journals.map { |journal| journal.user_id }.uniq
+		#   puts "user_ids: #{user_ids}, issue_id: #{issue_id}"
 
-		  user_ids.each do |user_id|
-			  hours = get_time_spent(user_id, issue.id)
-			  user = User.find(user_id).name
+		#   user_ids.each do |user_id|
+		# 	  hours = get_time_spent(user_id, issue.id)
+		# 	  user = User.find(user_id).name
 
-			  puts "user_id: #{user_id}, user: #{user}, hours: #{hours}"
-			  existing_user_entry = user_spent_time.find { |entry| entry[:user] == user }
+		# 	  puts "user_id: #{user_id}, user: #{user}, hours: #{hours}"
+		# 	  existing_user_entry = user_spent_time.find { |entry| entry[:user] == user }
 
-			  if existing_user_entry
-				existing_user_entry[:total_hours] += hours
-			  else
-				user_spent_time << {
-				  user: user,
-				  total_hours: hours
-				}
-			  end
-		  end
+		# 	  if existing_user_entry
+		# 		existing_user_entry[:total_hours] += hours
+		# 	  else
+		# 		user_spent_time << {
+		# 		  user: user,
+		# 		  total_hours: hours
+		# 		}
+		# 	  end
+		#   end
 
 		  issue_data = {
 			id: issue_id,
@@ -51,7 +52,6 @@ class IssueLifecycleController < ApplicationController
 			  save_elapsed_time_status(issue_data, status, elapsed_time_since_creation)
 			end
 
-		#   issue_data[:total_elapsed_time] = issue_data[:elapsed_time_status].sum { |status| status[:elapsed_time] }
 		  issue_data[:total_elapsed_hours] = issue_data[:elapsed_time_status].sum { |status| status[:elapsed_hours] }
 		  issue_data[:total_elapsed_minutes] = issue_data[:elapsed_time_status].sum { |status| status[:elapsed_minutes] }
 		  issues_data << issue_data
@@ -60,14 +60,13 @@ class IssueLifecycleController < ApplicationController
 		total_elapsed_time_category = issues_data.group_by { |issue| issue[:category] }.map do |category, issues|
 		  {
 			category: category,
-			# total_elapsed_time: issues.sum { |issue| issue[:total_elapsed_time] }
 			issue_ids: issues.map { |issue| issue[:id] },
 			total_elapsed_hours: issues.sum { |issue| issue[:total_elapsed_hours] },
 			total_elapsed_minutes: issues.sum { |issue| issue[:total_elapsed_minutes] }
 		  }
 		end
 		  respond_to do |format|
-			  format.json { render json: { issues_data: issues_data, total_elapsed_time_category: total_elapsed_time_category, user_spent_time: user_spent_time } }
+			  format.json { render json: { issues_data: issues_data, total_elapsed_time_category: total_elapsed_time_category, user_spent_time: user_spent_time, project_name: project_name } }
 		  end
 	end
 
@@ -141,5 +140,76 @@ end
 def get_time_spent(user_id, issue_id)
 	return  (TimeEntry.where(user_id: user_id, issue_id: issue_id).sum(:hours))
 end
+
+
+def project_details
+	project_id = @project.id
+
+	data = {}
+	data[:users] = []
+
+	begin
+	  @project_users = User.joins(:members).where(members: { project_id: project_id })
+
+	  if @project_users.any?
+		@project_users.each do |user|
+		  time_entries = TimeEntry.where(user_id: user.id, project_id: project_id)
+
+		  user_data = {
+			name: user.name,
+			time_entries: [],
+			total_hours: 0
+		  }
+
+		#   puts "User: #{user.name}"
+
+		#   puts 'TIME ENTRIES'
+		  time_entries.each do |time_entry|
+			# puts "User ID: #{time_entry.user_id}"
+			# puts "Hours: #{time_entry.hours}"
+
+			user_data[:total_hours] += time_entry.hours
+
+			if time_entry.issue_id.present?
+			  associated_issue = Issue.find(time_entry.issue_id)
+			#   puts "Associated Issue ID: #{associated_issue.id}"
+			  # Diğer Issue özelliklerini buraya ekleyebilirsiniz
+			else
+			#   puts "Bu TimeEntry, bir Issue ile ilişkilendirilmemiştir."
+			end
+
+			user_data[:time_entries] << {
+			  hours: time_entry.hours,
+			  issue_id: time_entry.issue_id
+			}
+		  end
+
+		  data[:users] << user_data
+		end
+
+		# Tüm kullanıcıları ve bilgilerini yazdırma
+		# puts 'TÜM KULLANICILAR VE BİLGİLERİ'
+	# 	data[:users].each do |user_data|
+	# 	  puts "User: #{user_data[:name]}"
+	# 	  puts "Toplam Saat: #{user_data[:total_hours]}"
+
+	# 	  user_data[:time_entries].each do |time_entry|
+	# 		puts "Hours: #{time_entry[:hours]}"
+	# 		puts "Issue ID: #{time_entry[:issue_id]}"
+	# 	  end
+
+	# 	  puts '-' * 20 # Kullanıcı bilgilerini ayırmak için bir çizgi ekledik
+	# 	end
+	  else
+		puts 'User not found in this project.'
+	  end
+
+	  return data
+	rescue => e
+	  puts "Error: #{e.message}"
+	  # Eğer bir hata oluştuysa, e.message ile hatanın açıklamasını alabilirsiniz
+	end
+  end
+
 
 end
--- assets/javascripts/issue_lifecycle.js
+++ assets/javascripts/issue_lifecycle.js
@@ -13,30 +13,128 @@ document.addEventListener('DOMContentLoaded', async function () {
 		let dataContainer = document.getElementById('dataContainer');
 		dataContainer.innerHTML = await issueTableHTML(data.issues_data);
 
+
 		$(document).ready(function () {
-			new DataTable('#issuesTable', {
+			let a = new DataTable('#issuesTable', {
 				info: false,
 				paging: false,
 				scrollCollapse: true,
 				scrollY: '95vh',
 				search: {
 					return: true
-				}
+				},
+				// columns: [
+				// 	null,null,null,
+				// 	{ orderable: false },null
+				// ],
 			});
-		});
-		createCategoryChart(data.issues_data);
+			// a.on('order.dt', function (e, settings, order) {
+				// 	if (order && order.length > 0 && order[0].col === 2) {
+					// 		console.log("OZEL SIRALAMA")
+					// 		// Burada özel sıralama mantığını tanımlayın
+					// 		// Örneğin, bir özel veri özelliği veya veri kullanarak sıralama yapabilirsiniz
+					// 		// Örneğin, özel bir veri özelliğiniz 'data-custom-sort' ise şu şekilde kullanabilirsiniz:
+			// 		// order[0][0] = { attr: 'data-custom-sort', order: 'asc' };
+			// 		order[0][0] = { data: 2, type: 'elapsed-time' };
+			// 	}
+			// });
+			// $.fn.dataTable.ext.order['elapsed-time'] = function (settings, col) {
+				// 	return this.api().column(col, { order: 'index' }).nodes().map(function (td, i) {
+					// 		const elapsedData = $(td).find('.issue-data').data('issue');
+					// 		return elapsedData ? elapsedData.reduce((sum, entry) => sum + entry.elapsed_time, 0) : 0;
+					// 	});
+					// };
+				});
 
-		dataContainer = document.getElementById('categoryTableContainer');
-		dataContainer.innerHTML = await categoryTableHTML(data.total_elapsed_time_category);
+				dataContainer = document.getElementById('projectName');
+				dataContainer.innerHTML = data.project_name;
+
+				createCategoryChart(data.issues_data);
+
+				dataContainer = document.getElementById('categoryTableContainer');
+				dataContainer.innerHTML = await categoryTableHTML(data.total_elapsed_time_category);
+
+				console.log(data.user_spent_time);
+				dataContainer = document.getElementById('userTableContainer');
+				dataContainer.innerHTML = await userTableHTML(data.user_spent_time);
+				createUserChart(data.user_spent_time);
+
+		dataContainer = document.getElementById('totalSpentTime');
+		dataContainer.innerHTML = await userTSTHTML(data.user_spent_time.users);
+
 	} catch (error) {
 		console.error("Data fetch Error!:", error);
 	}
 
+	async function userTSTHTML(data)
+	{
+		let totalTST = 0;
+		data.forEach(element => {
+			totalTST += element.total_hours;
+		})
+		console.log(totalTST);
+		let ttHTML = translations.label_total_spent_time + totalTST + translations.label_hour;
+		return ttHTML;
+	}
+
+	async function createUserChart(user_st)
+	{
+		let labels = [];
+		let total = [];
+		user_st.users.forEach(user => {
+			labels.push(user.name);
+			total.push(user.total_hours);
+		});
+        const data = {
+          labels: labels,
+          datasets: [{
+            data: total,
+            backgroundColor: await randomColors(labels),
+          }]
+        }
+        const options = {
+          responsive: true,
+          maintainAspectRatio: false,
+        }
+		createChart('doughnut', data, options, null, 'userChart');
+	}
+
+	async function userTableHTML(user_st)
+	{
+		let userStHTML = '<table class="table table-dark display table-striped" style="width:100%">';
+		userStHTML += '<thead class="thead-dark"><tr><th class="thCl" data-sortable="true">' + translations.label_user + '</th>';
+		userStHTML += '<th class="thCl">' + translations.label_issue_numbers + '</th>';
+		userStHTML += '<th class="thCl" data-sortable="true">' + translations.label_spent_time + '</th></tr></thead>';
+		userStHTML += '<tbody>';
+		user_st.users.forEach(function (user) {
+				userStHTML += '<td>' + (user.name || '-') + '</td>';
+				userStHTML += '<td class="isId" data-issue=\'' + JSON.stringify(user.time_entries) + '\'>';
+				user.time_entries.forEach(function (i, index) {
+					if (index < 3) {
+						userStHTML += '<strong>' + translations.label_issue + ' #' + i.issue_id + ' : ' + '</strong>' + i.hours + translations.label_hour + '<br>';
+					} else {
+						userStHTML += '<span class="additional-status" style="display: none;">' + '#' + i.issue_id + ' : ' + '</strong>' + i.hours + translations.label_hour + '<br></span>';
+					}
+				});
+				if (user.time_entries.length > 3) {
+					userStHTML += '<button class="show-more-btn btn" data-toggle="collapse" data-target=".additional-status">...</button>';
+				}
+				userStHTML += '</td>'
+				userStHTML += '<td class="totalHours">'
+				userStHTML += user.total_hours + ' saat' ;
+				userStHTML += '</td>';
+				userStHTML += '</tr>';
+			});
+			userStHTML += '</tbody>';
+			userStHTML += '</table>';
+			return userStHTML;
+	}
+
 	async function issueTableHTML(issues_data)
 	{
-		let html = '<table id="issuesTable" class="table-striped cell-border" style="width:100%">';
+		let html = '<table id="issuesTable" class="table-dark display table-striped" style="width:100%">';
 		html += '<thead class="thead-dark" style="font-size:large;"><tr>';
-		html += '<th data-sortable="true">'+ translations.label_issue_number + '</th>';
+		html += '<th>'+ translations.label_issue_number + '</th>';
 		html += '<th>' + translations.label_current_status + '</th>';
 		html += '<th>' + translations.label_elapsed_time_for_statuses + '</th>';
 		html += '<th style="text-align:center;">' + translations.label_status_changes + '</th>';
@@ -47,10 +145,13 @@ document.addEventListener('DOMContentLoaded', async function () {
 			html += '<td class="isId2">' + issue.id + '</td>';
 			html += '<td class="isId">' + (issue.status_now || '-') + '</td>';
 			html += '<td class="issue-data" data-issue=\'' + JSON.stringify(issue.elapsed_time_status) + '\'>';
+			issue.elapsed_time_status.sort(function(a, b) {
+				return a.elapsed_hours - b.elapsed_hours || a.elapsed_minutes - b.elapsed_minutes;
+			});
 			issue.elapsed_time_status.forEach(function (elapsed_time, index) {
 				var formattedTime = formatTime(elapsed_time.elapsed_hours, elapsed_time.elapsed_minutes);
 				if (index < 2) {
-					html += '<strong>' + elapsed_time.status_name + ':</strong> ' + formattedTime + '<br>';
+					html += '<strong>' + elapsed_time.status_name + ': </strong> ' + formattedTime + '<br>';
 				} else {
 					html += '<span class="additional-time" style="display: none;"><strong>' + elapsed_time.status_name + ':</strong> ' + formattedTime + '<br></span>';
 				}
@@ -76,7 +177,6 @@ document.addEventListener('DOMContentLoaded', async function () {
 		});
 		html += '</tbody>';
 		html += '</table>';
-		//html += '<p>User Spent Time: ' + data.user_spent_time + ' saat</p>';
 		return html;
 	}
 
@@ -92,7 +192,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 				categoryHtml += '<td class="isId" data-issue=\'' + JSON.stringify(category.issue_ids) + '\'>';
 				category.issue_ids.forEach(function (id, index) {
 					if (index < 3) {
-						categoryHtml += '<strong>' + id + '</strong>' + '<br>';
+						categoryHtml += '<strong>'+ '# ' + id + '</strong>' + '<br>';
 					} else {
 						categoryHtml += '<span class="additional-status" style="display: none;">' + id + '<br></span>';
 					}
@@ -110,7 +210,6 @@ document.addEventListener('DOMContentLoaded', async function () {
 			categoryHtml += '</tbody>';
 			categoryHtml += '</table>';
 
-			// document.getElementById('categoryTableContainer').innerHTML = categoryHtml;
 			return categoryHtml;
 	}
 
@@ -143,7 +242,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 			plugins: {
 				title: {
 					display: true,
-					text: 'Chart.js Bar Chart - Stacked',
+					// text: 'Chart.js Bar Chart - Stacked',
 				},
 			},
 			responsive: true,
@@ -181,8 +280,10 @@ document.addEventListener('DOMContentLoaded', async function () {
 	{
 		let ctx = document.createElement('canvas').getContext('2d');
 		if (element === 'categoryChart')
-		document.getElementById('categoryChart').appendChild(ctx.canvas);
-	else
+			document.getElementById('categoryChart').appendChild(ctx.canvas);
+		else if (element === 'userChart')
+			document.getElementById('userChart').appendChild(ctx.canvas);
+		else
 			chartContainer.appendChild(ctx.canvas);
 
 		new Chart(ctx, {
